689c5b1ea551df5dfbae6ebd8b12bd6430418f97
sillypantscoder <78504018+sillypantscoder@users.noreply.github.com>
2023-11-15 00:49:49 +0000

    Added testing files
    Also: updated card icons, got checkmark file ready, added todo file, fixed minor bugs

---
+++ test/test_client.py
@@ -0,0 +1,218 @@
+from getch import getch
+import typing
+import requests
+
+RESET = "\u001b[0m"
+RED = "\u001b[31m"
+YELLOW = "\u001b[33m"
+GREEN = "\u001b[32m"
+
+CURSOR_UP    = lambda n: f"\u001b[{n}A"
+CURSOR_DOWN  = lambda n: f"\u001b[{n}B"
+CURSOR_RIGHT = lambda n: f"\u001b[{n}C"
+CURSOR_LEFT  = lambda n: f"\u001b[{n}D"
+
+DELETE_LINE = "\r\u001b[2K"
+
+def call_addplayer(args: list[str]):
+	requests.post("http://0.0.0.0:8080/join_game", data=args[0].encode("UTF-8"), timeout=10)
+
+def call_ready(args: list[str]):
+	requests.post("http://0.0.0.0:8080/ready", data=args[0].encode("UTF-8"), timeout=10)
+
+def call_submitplan(args: list[str]):
+	requests.post("http://0.0.0.0:8080/submit_plan", data='\n'.join(args).encode("UTF-8"), timeout=10)
+
+commands: list[
+		# name and func
+	tuple[str, typing.Callable[[ list[str] ], typing.Any],
+		# syntax
+		list[
+			tuple[str, list[str] | type[int] | type[str]]
+		]
+	]
+] = [
+	("addplayer", call_addplayer, [
+		("playername", str)
+	]),
+	("ready", call_ready, [
+		("playername", str)
+	]),
+	("submitplan", call_submitplan, [
+		("playername", str),
+		("card1", ["forwards", "turn", "changeLevel", "shoot", "revenge"]),
+		("card2", ["forwards", "turn", "changeLevel", "shoot", "revenge"]),
+		("card3", ["forwards", "turn", "changeLevel", "shoot", "revenge"])
+	])
+	# ("test", call_addplayer, [
+	# 	("testy1", ["a", "bbbbbbb", "c"]),
+	# 	("testy2", str),
+	# 	("testy3", int)
+	# ])
+]
+
+def get_command() -> list[str]:
+	currentS: str = ""
+	selectedComplete: int = 0
+	def getSyntax(cmdname: str) -> "list[tuple[str, list[str] | type[int] | type[str]]]":
+		for cmd in commands:
+			if cmdname == cmd[0]:
+				return cmd[2]
+		return []
+	def getSyntaxString() -> str:
+		if len(currentS.split(" ")) <= 1: return ""
+		s = currentS.split(" ")
+		syntax = getSyntax(s[0])
+		syntax = syntax[len(s) - 2:]
+		result: list[str] = []
+		for item in syntax:
+			if isinstance(item[1], list):
+				result.append(f"<{item[0]}>")
+			elif isinstance(item[1](), str):
+				result.append(f"<{item[0]}: string>")
+			elif isinstance(item[1](), int):
+				result.append(f"<{item[0]}: number>")
+		if len(result) == 0: return ""
+		padding = " " * (len(' '.join(s)) - (len(s[-1]) - 2))
+		firstIPad = YELLOW + result[0].ljust(max(len(result[0]), len(s[-1])) + 1) + RESET
+		return padding + firstIPad + " ".join(result[1:])
+	def validate(cmdname: str, itemno: int, item: str):
+		if itemno - 1 < 0: return cmdname in [c[0] for c in commands]
+		syntax = getSyntax(cmdname)
+		if itemno - 1 >= len(syntax): return False
+		argument = syntax[itemno - 1][1]
+		if isinstance(argument, list):
+			return item in argument
+		elif isinstance(argument(), str):
+			return True
+		elif isinstance(argument(), int):
+			for char in item:
+				if char not in [*"0123456789"]:
+					return False
+			return True
+	def getComplete():
+		results: list[str] = []
+		s = currentS.split(" ")
+		if len(s) == 1: return [c[0] for c in commands]
+		# print(getSyntax(s[0]), len(s), "\n", repr(s), "\n", sep="")
+		syntax = getSyntax(s[0])[len(s) - 2][1]
+		if isinstance(syntax, list):
+			results.extend(syntax)
+		# print(repr(results) + "\n\n")
+		return results
+	def getFilteredComplete():
+		results: list[str] = []
+		pre: list[str] = getComplete()
+		alreadyTyped = currentS.split(" ")[-1]
+		for item in pre:
+			if item.startswith(alreadyTyped):
+				results.append(item)
+		return results
+	def printState():
+		splitCmd = currentS.split(" ")
+		# Syntax
+		cmdname = splitCmd[0]
+		if len(splitCmd) > 1:
+			print(getSyntaxString())
+		else:
+			print()
+		# Current string placeholder
+		print()
+		# Examples
+		n = 1
+		for i, ex in enumerate(getFilteredComplete()):
+			n += 1
+			padding = " " * (len(' '.join(splitCmd)) - (len(splitCmd[-1]) - 2))
+			print(padding + (YELLOW if i == selectedComplete else "") + ex + RESET)
+		# Current string (with validation)
+		n += 1
+		print(CURSOR_UP(n - 1), end=">")
+		for i in range(len(splitCmd)):
+			part = splitCmd[i]
+			print(end=" ")
+			if validate(cmdname, i, part):
+				print(GREEN+part+RESET, end="")
+			else:
+				print(RED+part+RESET, end="")
+		return n
+	def eraseState(n):
+		print(CURSOR_DOWN(n - 1), end="")
+		for _ in range(n):
+			print(DELETE_LINE, end="")
+			print(CURSOR_UP(1), end="")
+		print(DELETE_LINE, end="")
+	while True:
+		n_lines = printState()
+		char = getch()
+		if char in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789":
+			currentS += char
+			selectedComplete = 0
+		elif char == "\x7f":
+			# delete
+			currentS = currentS[:-1]
+			selectedComplete = 0
+		elif char == "\r":
+			# enter
+			eraseState(n_lines)
+			print("\n> " + currentS + "\n")
+			return currentS.split(" ")
+		elif char == "\t":
+			complete = getFilteredComplete()
+			if selectedComplete < len(complete):
+				currentS = currentS[:currentS.rfind(" ") + 1] + complete[selectedComplete] + " "
+			selectedComplete = 0
+		elif char == "\x1b":
+			# esc
+			getch()
+			char2 = getch()
+			if char2 == "B":
+				# down
+				selectedComplete += 1
+				maxC = len(getFilteredComplete()) - 1
+				# print("\n", "\n", f"a:{selectedComplete},{maxC}:a", "\n", "\n", "\n", sep="\n")
+				if selectedComplete >= maxC: selectedComplete = maxC
+			elif char2 == "A":
+				selectedComplete -= 1
+				if selectedComplete < 0: selectedComplete = 0
+			else:
+				print(repr(char))
+		else:
+			print(repr(char))
+		eraseState(n_lines)
+
+def read_cmd_from_file():
+	f = open("alice_murders_bob_brutally_hahahaha.txt", "r")
+	t = f.read().split("\n")
+	f.close()
+	nextLine = 0
+	for lineno in range(len(t)):
+		if t[lineno].startswith("> "):
+			t[lineno] = t[lineno][2:]
+			nextLine = lineno + 1
+	while t[nextLine].startswith("#"): nextLine += 1
+	t[nextLine] = "> " + t[nextLine]
+	f = open("alice_murders_bob_brutally_hahahaha.txt", "w")
+	f.write("\n".join(t))
+	f.close()
+	exec_cmd(t[nextLine][2:].split(" "))
+	print(f"[From file] {t[nextLine][2:]}")
+
+def exec_cmd(cmd: list[str]):
+	func = None
+	for ex in commands:
+		if ex[0] == cmd[0]:
+			func = ex[1]
+
+	if isinstance(func, typing.Callable):
+		func(cmd[1:])
+	else:
+		print("Invalid command")
+
+def main():
+	while True:
+		cmd = get_command()
+		if len(cmd) == 1 and cmd[0] == '':
+			read_cmd_from_file()
+		else:
+			exec_cmd(cmd)
+main()
---
+++ test/testserver.py
@@ -0,0 +1,157 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import typing
+import os
+import json
+from game import Game
+
+hostName = "0.0.0.0"
+serverPort = 8080
+
+class URLQuery:
+	def __init__(self, q):
+		self.orig = q
+		self.fields = {}
+		for f in q.split("&"):
+			s = f.split("=")
+			if len(s) >= 2:
+				self.fields[s[0]] = s[1]
+	def get(self, key):
+		if key in self.fields:
+			return self.fields[key]
+		else:
+			return ''
+
+def read_file(filename: str) -> bytes:
+	"""Read a file and return the contents."""
+	f = open(filename, "rb")
+	t = f.read()
+	f.close()
+	return t
+
+def write_file(filename: str, content: bytes):
+	"""Write data to a file."""
+	f = open(filename, "wb")
+	f.write(content)
+	f.close()
+
+class HttpResponse(typing.TypedDict):
+	"""A dict containing an HTTP response."""
+	status: int
+	headers: dict[str, str]
+	content: str | bytes
+
+game: Game = Game()
+write_file("game_ref.json", json.dumps(game.toDict()).encode("UTF-8"))
+
+def get(path: str, query: URLQuery) -> HttpResponse:
+	# playername = query.get("name")
+	if os.path.isfile("public_files" + path):
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": {
+					"html": "text/html",
+					"js": "text/javascript",
+					"css": "text/css",
+					"svg": "image/svg+xml"
+				}[path.split(".")[-1]]
+			},
+			"content": read_file("public_files" + path)
+		}
+	elif os.path.isdir("public_files" + path):
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": "text/html"
+			},
+			"content": read_file("public_files" + path + "index.html")
+		}
+	elif path == "/status":
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": "application/json"
+			},
+			"content": read_file("game.json")
+		}
+	else: # 404 page
+		print("404 GET " + path)
+		return {
+			"status": 404,
+			"headers": {
+				"Content-Type": "text/html"
+			},
+			"content": ""
+		}
+
+def post(path: str, body: bytes) -> HttpResponse:
+	bodydata = body.decode("UTF-8").split("\n")
+	if path == "/join_game":
+		return {
+			"status": 501,
+			"headers": {},
+			"content": ""
+		}
+	elif path == "/ready":
+		return {
+			"status": 501,
+			"headers": {},
+			"content": ""
+		}
+	elif path == "/submit_plan":
+		return {
+			"status": 501,
+			"headers": {},
+			"content": ""
+		}
+	else:
+		print("404 POST " + path)
+		return {
+			"status": 404,
+			"headers": {
+				"Content-Type": "text/html"
+			},
+			"content": ""
+		}
+
+class MyServer(BaseHTTPRequestHandler):
+	def do_GET(self):
+		splitpath = self.path.split("?")
+		res = get(splitpath[0], URLQuery(''.join(splitpath[1:])))
+		self.send_response(res["status"])
+		for h in res["headers"]:
+			self.send_header(h, res["headers"][h])
+		self.end_headers()
+		c = res["content"]
+		if isinstance(c, str): c = c.encode("utf-8")
+		self.wfile.write(c)
+	def do_POST(self):
+		res = post(self.path, self.rfile.read(int(self.headers["Content-Length"])))
+		self.send_response(res["status"])
+		for h in res["headers"]:
+			self.send_header(h, res["headers"][h])
+		self.end_headers()
+		c = res["content"]
+		if isinstance(c, str): c = c.encode("utf-8")
+		self.wfile.write(c)
+	def log_message(self, _format: str, *args) -> None:
+		return
+		# if 400 <= int(args[1]) < 500:
+		# 	# Errored request!
+		# 	print("\u001b[31m", end="")
+		# print(args[0].split(" ")[0], "request to", args[0].split(" ")[1], "(status code:", args[1] + ")")
+		# print("\u001b[0m", end="")
+		# # don't output requests
+
+if __name__ == "__main__":
+	running = True
+	webServer = HTTPServer((hostName, serverPort), MyServer)
+	webServer.timeout = 1
+	print(f"Server started http://{hostName}:{serverPort}")
+	while running:
+		try:
+			webServer.handle_request()
+		except KeyboardInterrupt:
+			running = False
+	webServer.server_close()
+	print("Server stopped")
